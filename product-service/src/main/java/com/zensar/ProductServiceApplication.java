d0df99cb1e2c2c9d53cfc944993e2228cff716f1
sharad <sharad.rajore@gmail.com>
2021-05-13 11:57:25 +0530

    changed hostname property value

--- product-service/src/main/java/com/zensar/ProductServiceApplication.java
+++ product-service/src/main/java/com/zensar/ProductServiceApplication.java
@@ -1,9 +1,11 @@
 package com.zensar;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,9 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductServiceApplication {
 	
+	@Value("${eureka.instance.hostname}")
+	private String hostName;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+	
+	@GetMapping("/config")
+	public String getConfiguration() {
+		return hostName;
 	}
 	
 	
