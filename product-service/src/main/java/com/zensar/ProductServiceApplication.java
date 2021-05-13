abefe2a69a245c30c83540eb17146f56a94cdb76
sharad <sharad.rajore@gmail.com>
2021-05-13 11:24:13 +0530

    config server works

--- product-service/src/main/java/com/zensar/ProductServiceApplication.java
+++ product-service/src/main/java/com/zensar/ProductServiceApplication.java
@@ -4,20 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@RestController
 public class ProductServiceApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 	
+	
+	
+	
 	/*
 	 * @Bean public RestTemplate restTemplate() { return new RestTemplate(); }
 	 */
+	
+	
 
 }
--- product-service/src/main/java/com/zensar/controllers/ProductResource.java
+++ product-service/src/main/java/com/zensar/controllers/ProductResource.java
@@ -14,26 +14,33 @@ import com.zensar.services.ProductService;
 @RestController
 @RequestMapping("/productapi")
 public class ProductResource {
-
-	//@Autowired
-	//private RestTemplate restTemplate;
 	
+	
+
+	// @Autowired
+	// private RestTemplate restTemplate;
+
 	@Autowired
 	private CouponClient client;
 
 	@Autowired
 	private ProductService service;
+
 // http://localhost:8080/productapi/products  ----> POST
 	@PostMapping("/products")
 	public Product createProduct(@RequestBody Product product) {
-		
-		Coupon coupon=client.getCoupon(product.getCouponCode());
 
-		//Coupon coupon = restTemplate.getForObject("http://localhost:8082/couponapi/coupons/CRICKET", Coupon.class);
+		Coupon coupon = client.getCoupon(product.getCouponCode());
+
+		// Coupon coupon =
+		// restTemplate.getForObject("http://localhost:8082/couponapi/coupons/CRICKET",
+		// Coupon.class);
 
 		product.setProductPrice(product.getProductPrice() - (coupon.getDiscount()));
 
 		return service.insertProduct(product);
 	}
+
+	
 
 }
