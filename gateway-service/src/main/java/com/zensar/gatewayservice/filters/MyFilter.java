65817c1473a8829060f1afb843e5cca8602e8c85
sharad <sharad.rajore@gmail.com>
2021-05-13 13:00:16 +0530

    final commit

---
+++ gateway-service/src/main/java/com/zensar/gatewayservice/filters/MyFilter.java
@@ -0,0 +1,21 @@
+package com.zensar.gatewayservice.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class MyFilter implements GlobalFilter {
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		System.out.println("Pre processing logic goes here !!!!");
+		return chain.filter(exchange).then(Mono.fromRunnable(()->{
+			System.out.println("Post processing logic goes here");
+		}));
+	}
+
+}
