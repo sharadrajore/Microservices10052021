37de6504aae73135091d6a04451feecf2911b62f
sharad <sharad.rajore@gmail.com>
2021-05-13 12:03:55 +0530

    added hostname property

--- product-service/src/main/java/com/zensar/ProductServiceApplication.java
+++ product-service/src/main/java/com/zensar/ProductServiceApplication.java
@@ -3,6 +3,7 @@ package com.zensar;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient
 @EnableFeignClients
 @RestController
+@RefreshScope
 public class ProductServiceApplication {
 	
 	@Value("${eureka.instance.hostname}")
